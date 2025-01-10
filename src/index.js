04dc75e8d411535819fc6aea35cd8df212825338
palma-t <184235218+palma-t@users.noreply.github.com>
2025-01-10 18:42:04 +0100

    Add comments on structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
 
 
+/* les fonctions dont on va avoir besoin (réfléchir aux classes après)
+- add a task
+- today: look for all tasks with a deadline today and show them
+- upcoming: same but for 3 coming days
+- add a project
+- list all projects
+- sur ce doc: les addeventlisteners pour tout le nav
+*/
