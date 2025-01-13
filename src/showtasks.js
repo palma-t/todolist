881eb418a3ff05e5043f741244f99462c2d824c6
palma-t <184235218+palma-t@users.noreply.github.com>
2025-01-13 10:49:09 +0100

    Create to manage displaying tasks

---
+++ src/showtasks.js
@@ -0,0 +1,23 @@
+// besoin de creer un objet avec toutes les classes dedans ? 
+// peut-etre pas mal pour toutes les affiches
+
+function showTasks() {
+
+}
+
+function showTodayTasks() {
+
+
+}
+
+function showUpcomingTasks() {
+
+}
+
+
+function showOverdueTasks() {
+
+}
+
+
+export { showTasks, showTodayTasks, showUpcomingTasks, showOverdueTasks }
