a1502bc102bdad6f1edfb69206e74e7ceeeefb99
palma-t <184235218+palma-t@users.noreply.github.com>
2025-01-13 17:52:59 +0100

    Add deleting options for tasks

--- src/showtasks.js
+++ src/showtasks.js
@@ -1,10 +1,15 @@
+import { allTasksArray } from "./taskconstructor.js";
+
 function showTasks(array) {
     let content = document.querySelector("#content");
     let allTaskTitle = document.createElement("h2");
     allTaskTitle.innerText = "All tasks";
     content.appendChild(allTaskTitle);
-    
-    for(const task of array) {
+
+    let gridDiv = document.createElement("div");
+    gridDiv.classList.add("task-grid")
+
+    for(let task of array) {
         let taskSquare = document.createElement("div");
         let taskTitle = document.createElement("h3");
         taskTitle.innerText = task.title;
@@ -12,12 +17,24 @@ function showTasks(array) {
         let taskDescription = document.createElement("p");
         taskDescription.innerText = task.description;
 
+        let del = document.createElement("button");
+        del.innerText = "Del"
+        del.classList.add("del");
+
+        del.addEventListener("click", function(){
+            allTasksArray.deleteTask(task);
+            //deleteTask a definir
+            //delete du projet?
+        }); 
+
         taskSquare.classList.add("task-style");
         taskSquare.appendChild(taskTitle);
         taskSquare.appendChild(taskDescription);
 
-        content.appendChild(taskSquare);
+        gridDiv.appendChild(taskSquare);
     }
+
+    content.appendChild(gridDiv);
 }
 
 function showTodayTasks() {
